17cda4763b53977e7e12cb3e012783f4c38a5065
Jose Laruta <eduardo.laruta@gmail.com>
2021-07-30 20:03:31 -0400

    clean up

--- rpi_robot_navigation/launch/rpi_robot_navigation.launch.py
+++ rpi_robot_navigation/launch/rpi_robot_navigation.launch.py
@@ -24,8 +24,6 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
-TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
-
 
 def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time', default='false')
@@ -46,10 +44,10 @@ def generate_launch_description():
 
     nav2_launch_file_dir = os.path.join(get_package_share_directory('nav2_bringup'), 'launch')
 
-    rviz_config_dir = os.path.join(
-        get_package_share_directory('nav2_bringup'),
-        'rviz',
-        'nav2_default_view.rviz')
+    # rviz_config_dir = os.path.join(
+    #     get_package_share_directory('nav2_bringup'),
+    #     'rviz',
+    #     'nav2_default_view.rviz')
 
     return LaunchDescription([
         DeclareLaunchArgument(
