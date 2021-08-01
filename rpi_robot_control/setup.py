79b3f1b4ebd89a1b86e302db0ed53290060e5e61
Jose Laruta <eduardo.laruta@gmail.com>
2021-08-01 19:20:21 -0400

    created robot control package

---
+++ rpi_robot_control/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'rpi_robot_control'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='pepe',
+    maintainer_email='eduardo.laruta@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
